2bcd13eb56ee99f6322e546020c4cc3a3e8d9173
drphil007 <philinewairata@gmail.com>
2019-02-27 21:45:33 +0100

    homework 3 all files, github index page

---
+++ homework/week_3/convertCSV2JSON.py
@@ -0,0 +1,42 @@
+"""
+Name: Philine Wairata
+Studentnumber: 10517863
+
+Converts CSV to JSON
+"""
+
+import csv
+import json
+import pandas as pd 
+
+# reads rows from csv and skips header
+data = pd.read_csv("input.csv",skiprows=10)
+
+# set variables for columns and clean data
+datum_column = data['YYYYMMDD']
+datum_column = datum_column.replace(',','.', regex=True).astype(float)
+
+hoogste_windstoot_column = data['FXX']
+hoogste_windstoot_column = hoogste_windstoot_column.replace(' ','', regex=True).astype(float)
+
+# set dicionary for json
+windstoot_per_datum = {}
+
+# order each datum with hoogste windstoot per column
+for index, row in data.iterrows():
+
+    windstoot_per_datum[index] = {}
+
+    try: 
+        windstoot_per_datum[index].update({"datum: " : datum_column[index]})
+    except:
+        windstoot_per_datum[index].update({"datum: " : "TEST"})
+    
+    try: 
+        windstoot_per_datum[index].update({"hoogeste_windstoot: " : hoogste_windstoot_column[index]})
+    except:
+        windstoot_per_datum[index].update({"hoogste_windstoot: " : "TEST"})
+
+# write data to json file
+with open('data_test.json', 'w') as outfile:
+    json.dump(windstoot_per_datum, outfile, indent=4, separators=(',', ': '))
